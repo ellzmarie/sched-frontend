7057908b9353b8c455d976a858330fa513170ecf
Ellie-Marie Matutis-Digal <ellie-mariematutis-digal@Ellie-Maries-MacBook-Air.local>
2023-09-04 14:37:09 -0700

    add components files

---
+++ Main.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+function Main(props) {
+  return (
+    <main>
+      <Routes>
+        <Route exact path="/" element={<Index />} />
+        <Route path="/contact/:id" element={<Show />} />
+      </Routes>
+  </main>
+  )
+}
+
+
+  
+
+export default Main
