7f1026892d259e1f869b334dd51e300a1543ab83
Ellie-Marie Matutis-Digal <ellie-mariematutis-digal@Ellie-Maries-MacBook-Air.local>
2023-09-04 14:58:11 -0700

    updates files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 // Make a component for Header and Main
+import { Link } from "react-router-dom"
 
-function Component(props) {
-    return <h1>Component Name</h1>
+function Header(props) {
+    return (
+      <nav className="nav">
+        <Link to="/">
+          <div>Contact App</div>
+        </Link>
+      </nav>
+    )
   }
   
   export default Component
